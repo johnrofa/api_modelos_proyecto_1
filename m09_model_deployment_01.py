ad23ceb24347402699216af5e5fc8d6cd8db70e3
johnrofa <johnrofa@hotmail.com>
2023-04-29 00:10:42 -0500

    Update m09_model_deployment_01.py

--- m09_model_deployment_01.py
+++ m09_model_deployment_01.py
@@ -1,54 +1,38 @@
 #!/usr/bin/python
-
 import pandas as pd
-import joblib
-import sys
-import os
 
-def predict_proba(url):
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBRegressor
 
-    try:
-    
-        regRF11 = joblib.load(os.path.dirname(__file__) + '/phishing_clf_01.pkl') 
-        leMake= joblib.load(os.path.dirname(__file__) + '/leMake_01.pkl')
-        leModel= joblib.load(os.path.dirname(__file__) + '/leModel_01.pkl')
-        leState= joblib.load(os.path.dirname(__file__) + '/leState_01.pkl')  
+def Modelos(datos1, leMake, leModel, leState, Modelo):
+
+    # Leer los datos post
+
+    Year = datos1["Year"]
+    Mileage = datos1["Mileage"]
+    State = datos1["State"]
+    Make = datos1["Make"]
+    Model = datos1["Model"]
+    ID = 0
+
+    columnas = ['Year', 'Mileage', 'State', 'Make', 'Model', 'ID']
+    datos = [[Year, Mileage, State, Make, Model, ID]]
+
+    domain_01 = pd.DataFrame(datos, columns=columnas)
+    db_02 = domain_01.set_index('ID')
+
+    print('db_02 :', db_02)
 
 
-        url_ = pd.DataFrame([url], columns=['url'])
-        domain = url_.url.str.split('/', expand=True)
-        domain.rename(columns={3: "Year", 4: "Mileage", 5: "State", 6: "Make", 7:"Model"}, inplace=True)
-        domain_01=domain.drop([0, 1, 2], axis=1)
-        domain_01['ID']=0
-        domain_02=domain_01.set_index('ID')
-    
-        #Transformación
-    
-        domain_02["State"]=leState.transform(domain_02.State)
-        domain_02["Make"]=leMake.transform(domain_02.Make)
-        domain_02["Model"]=leModel.transform(domain_02.Model)    
+    db_02["State"] = leState.transform(db_02.State)
+    db_02["Make"] = leMake.transform(db_02.Make)
+    db_02["Model"] = leModel.transform(db_02.Model)
 
-    
-        # Make prediction
-        ypredRF11 = regRF11.predict(domain_02)
-    except:
-        ypredRF11= -999
-    
-    
+    print('----------------realizar predicción----------------------')
+
+    # Make prediction
+    ypredRF11 = Modelo.predict(db_02)
+    ypredRF11 = str(ypredRF11[0])
+
     return ypredRF11
-
-
-if __name__ == "__main__":
-    
-    if len(sys.argv) == 1:
-        print('Please add an URL')
-        
-    else:
-
-        url = sys.argv[1]
-
-        ypredRF11 = predict_proba(url)
-        
-        print(url)
-        print('Costo del vehiculo: ', ypredRF11)
-        
