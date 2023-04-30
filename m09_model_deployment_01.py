dbb23296fc48ba5f9b795523fe01ead4212e2400
johnrofa <johnrofa@hotmail.com>
2023-04-30 14:11:19 -0500

    Update m09_model_deployment_01.py

--- m09_model_deployment_01.py
+++ m09_model_deployment_01.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python
 import pandas as pd
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBRegressor
 
+
+
 def Modelos(datos1, leMake, leModel, leState, Modelo):
-
-    # Leer los datos post
-
     Year = datos1["Year"]
     Mileage = datos1["Mileage"]
     State = datos1["State"]
@@ -22,17 +20,12 @@ def Modelos(datos1, leMake, leModel, leState, Modelo):
     domain_01 = pd.DataFrame(datos, columns=columnas)
     db_02 = domain_01.set_index('ID')
 
-    print('db_02 :', db_02)
-
-
     db_02["State"] = leState.transform(db_02.State)
     db_02["Make"] = leMake.transform(db_02.Make)
     db_02["Model"] = leModel.transform(db_02.Model)
 
-    print('----------------realizar predicción----------------------')
-
     # Make prediction
-    ypredRF11 = Modelo.predict(db_02)
-    ypredRF11 = str(ypredRF11[0])
+    ypred = Modelo.predict(db_02)
+    ypred = str(ypred[0])
 
-    return ypredRF11
+    return ypred
